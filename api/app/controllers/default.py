89bfa2bb5228d8e1ef77bee5a32b413ce9df6c36
Mekylei-Belchior <mekylei@msn.com>
2020-08-06 14:33:26 -0300

    defaut.py file checked with pylint

--- api/app/controllers/default.py
+++ api/app/controllers/default.py
@@ -1,29 +1,31 @@
+""" Imported Modules """
+from pickle import load
 from pandas import DataFrame
+from app import app
 
 from flask import request
-from pickle import load
-from app import app
 
 
 # Load pickle file model
 with open('C:/Users/mekyl/Documents/GitHub/api-machine-learning/model/model.pkl', 'rb') as file:
-	model = load(file)
+    model = load(file)
 
 
 @app.route('/api_predict', methods=['POST'])
 @app.route('/', methods=['POST'])
 def api_predict():
-	validation_json = request.get_json()
+    """ Route to API Predict """
+    validation_json = request.get_json()
 
-	if validation_json:
-		if isinstance(validation_json, dict):
-			df = DataFrame(validation_json, index=[0])
-		else:
-			df = DataFrame(validation_json, columns=validation_json[0].keys())
+    if validation_json:
+        if isinstance(validation_json, dict):
+            data = DataFrame(validation_json, index=[0])
+        else:
+            data = DataFrame(validation_json, columns=validation_json[0].keys())
 
 	# Prediction
-	pred = model.predict(df)
+    pred = model.predict(data)
 
-	df['prediction'] = pred
+    data['prediction'] = pred
 
-	return df.to_json( orient='records')
+    return data.to_json(orient='records')
