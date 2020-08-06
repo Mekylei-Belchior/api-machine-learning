8326a93869d1d7e67941d4934e7011c596187705
Mekylei-Belchior <mekylei@msn.com>
2020-08-06 13:56:34 -0300

    check files with pylint

--- model/model.py
+++ model/model.py
@@ -1,38 +1,31 @@
-from pandas import DataFrame
+""" Imported modules """
 from pickle import dump
+from pandas import DataFrame
 
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.datasets import load_iris
-
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 
 # Dataset loading
-iris = load_iris() 
+iris = load_iris()
 
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-df['target'] = iris.target
+df = DataFrame(iris['data'], columns=iris['feature_names'])
+df['target'] = iris['target']
 
 features = df.iloc[:, 0:4].values
 target = df['target'].values
 
 # Divide in data training and data test
-X_train, X_test, y_train, y_test = train_test_split(features,
-													target,
-													test_size=0.2,
-													random_state=9)
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=.2, random_state=9)
 
 # Simple tree
-tree = DecisionTreeClassifier(	max_depth=1,
-								splitter='random',
-								max_features='sqrt')
+tree = DecisionTreeClassifier(max_depth=1, splitter='random', max_features='sqrt')
 
 # Multi tree
-trees = BaggingClassifier(	base_estimator=tree,
-							n_estimators=1000,
-							bootstrap=False)
+trees = BaggingClassifier(base_estimator=tree, n_estimators=1000, bootstrap=False)
 
 # Model training
 model = trees.fit(X_train, y_train)
@@ -43,4 +36,4 @@ score = accuracy_score(y_test, pred)
 
 # Saving trained model
 with open('model.pkl', 'wb') as file:
-	dump(model, file)
+    dump(model, file)
