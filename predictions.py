31dbacf9874d82570dd46de980168e6311ede995
Mekylei-Belchior <mekylei@msn.com>
2020-08-06 13:40:09 -0300

    check with pylint

--- predictions.py
+++ predictions.py
@@ -1,16 +1,17 @@
-from sklearn.datasets import load_iris
+""" Modules import """
 
-from pandas import DataFrame
-from requests import post
 from json import loads
+from requests import post
+from pandas import DataFrame
 
+from sklearn.datasets import load_iris
 
 # Dataset loading
 iris = load_iris()
 
-features = iris.data
-labels = iris.feature_names
-target = iris.target
+features = iris['data']
+labels = iris['feature_names']
+target = iris['target']
 
 target_names = {0: 'setosa', 1: 'versicolor', 2: 'virginica'}
 
@@ -21,12 +22,12 @@ sample_json = sample.to_json(orient='records')
 
 
 # API parameters
-url = 'http://127.0.0.1:5000/api_predict'
+URL = 'http://127.0.0.1:5000/api_predict'
 data = sample_json
 header = {'Content-type': 'application/json'}
 
 # API response
-r = post(url=url, data=data, headers=header)
+r = post(url=URL, data=data, headers=header)
 
 data_dict = loads(data)[0]
 response = r.json()[0]
@@ -36,15 +37,15 @@ response = r.json()[0]
 print()
 
 # Print predict data
-print(f'#' * 20, f'{"Predict Data":^20}', f'#' * 20)
+print('#' * 20, f'{"Predict Data":^20}', '#' * 20)
 for k, v in data_dict.items():
-	print(f'{k}: {v}\n', end='')
+    print(f'{k}: {v}\n', end='')
 
-print(f'\n\n')
+print('\n\n')
 
 # Print predict response
-print(f'#' * 20, f'{"Predict Response":^20}', f'#' * 20)
+print('#' * 20, f'{"Predict Response":^20}', '#' * 20)
 for k, v in response.items():
-	print(f'{k}: {v}\n', end='')
+    print(f'{k}: {v}\n', end='')
 
 print(f'\nPredicted Class ({response["prediction"]}): {target_names[response["prediction"]]}')
